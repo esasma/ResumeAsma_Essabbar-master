3b8bf0f8333ea73f7dca6122887098f17c531939
asma es-sabbar <asma.es-sabbar@univ-fcomte.fr>
2023-02-06 18:15:04 +0100

    modifsvg

---
+++ src/components/logovuejs.vue
@@ -0,0 +1,13 @@
+<template>
+    <svg class="animate-bounce w-14 h-12 mt-6" width="28" height="25" viewBox="0 0 28 25" fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M22.4 0H28L14 24.15L0 0H10.71L14 5.6L17.22 0H22.4Z" fill="#41B883" />
+        <path d="M0 0L14 24.15L28 0H22.4L14 14.49L5.53 0H0Z" fill="#41B883" />
+        <path d="M5.53003 0L14 14.56L22.4 0H17.22L14 5.6L10.71 0H5.53003Z" fill="#35495E" />
+    </svg>
+
+</template>
+
+<script>
+
+</script>
---
+++ src/components/rond.vue
@@ -0,0 +1,10 @@
+<template>
+    <svg class="-mt-1 mx-3" width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="15.5" cy="15.5" r="13.5" stroke="black" stroke-width="4" />
+    </svg>
+
+</template>
+
+<script>
+
+</script>
