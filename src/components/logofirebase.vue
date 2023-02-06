7263c6a04f714b8ca649d5f53fbaf939cc86d603
asma es-sabbar <asma.es-sabbar@univ-fcomte.fr>
2023-02-06 18:13:50 +0100

    modifsvg

---
+++ src/components/logofirebase.vue
@@ -0,0 +1,11 @@
+<template>
+    <svg class="animate-bounce w-24 h-17" width="73" height="28" viewBox="0 0 73 28" fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+    </svg>
+
+</template>
+
+
+<script>
+
+</script>
