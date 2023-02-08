05e975cc5dabfa1502cffe08ff15b482d8aef50c
asma es-sabbar <asma.es-sabbar@univ-fcomte.fr>
2023-02-08 14:34:36 +0100

    modif

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header class=" ">
     <div class="mt-10">
-      <router-link to="/ResumeView"
+      <router-link to="/"
         class=" ml-80 m-10 uppercase bg-transparent hover:bg-black text-black font-semibold hover:text-white py-2 px-4 border-2 border-black hover:border-transparent rounded">
         Français
       </router-link>
@@ -19,15 +19,20 @@ import { RouterLink, RouterView } from 'vue-router'
   </header>
   <Router-View />
 
+  <div>
+
+  </div>
+
 </template>
 
 <script>
 import { MenuIcon, } from "@heroicons/vue/solid";
+import ResumeView from './views/ReseumeView.vue'
 
 
 export default {
   name: "App",
-  components: { MenuIcon, },
+  components: { MenuIcon, ResumeView },
 };
 
 </script>
